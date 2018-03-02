9ee88aaadaa337913e2a23dd623da381150b717a
Yuko <std.somebody@gmail.com>
2018-03-02 14:55:07 +0300

    error recovering init

--- app/middlewares.js
+++ app/middlewares.js
@@ -1,12 +1,27 @@
 import _get from 'lodash/fp/get'
+import _omit from 'lodash/fp/omit'
+import { createAction, createFailureActionName } from './modules/action.helper'
 
 const getErrorCode = _get('payload.errorCode')
+const omitErrorCode = _omit('errorCode')
+const isGlobalError = action => getErrorCode(action) < 0
+const globalErrorAction = createAction(createFailureActionName('connection')('app'))
+
+function forwardError(store, { payload }) {
+  store.dispatch(globalErrorAction(omitErrorCode(payload)))
+}
+
+function repeatAction(store, { prevAction: { executeWithDelay = 3, ...prevAction } }) {
+  store.dispatch({
+    ...prevAction,
+    executeWithDelay: Math.min(executeWithDelay * 2, 60)
+  })
+}
 
 export const catchErrors = store => next => (action) => {
-  if (getErrorCode(action) < 0) {
-    store.dispatch({
-      type: 'LOL'
-    })
+  if (isGlobalError(action)) {
+    forwardError(store, action)
+    repeatAction(store, action)
   }
   return next(action)
 }
