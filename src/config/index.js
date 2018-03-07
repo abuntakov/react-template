72254dec6a526e42f8ce999d3e058c0329a9c2b4
Yuko <std.somebody@gmail.com>
2018-03-07 16:22:10 +0300

    test config

--- src/config/index.js
+++ src/config/index.js
@@ -3,6 +3,6 @@ const path = require('path')
 
 nconf.argv()
   .env('__')
-  .file({ file: path.join(__dirname, 'config.json') })
+  .file({ file: path.join(__dirname, process.env.NODE_ENV === 'production' ? 'config.json' : 'test.json') })
 
 module.exports = nconf
